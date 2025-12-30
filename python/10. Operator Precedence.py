02a7d0f6ed8c21792c5e1690b952d242c1bd8be6
Pranay <praveenpranay07@gmail.com>
2025-12-30 11:36:10 +0530

    Learnt about Operator Precedence.

---
+++ python/10. Operator Precedence.py
@@ -0,0 +1,15 @@
+'''
+Operator Precedence:-
+
+1. ()
+2. **
+3. *, /, //, %
+4. +, -
+5. <, >, <=, >=, ==, !=
+6. not
+7. and
+8. or
+
+If two operators have the same precedence, the one on the left is evaluated first.
+
+'''
