1447a7c659b200e83ce2d09c6e74d4997a4fed88
Pranay <praveenpranay07@gmail.com>
2025-12-30 11:22:50 +0530

    Learnt about the different Assignment Operators

---
+++ python/8. Assignment Operators.py
@@ -0,0 +1,22 @@
+a = 10
+
+a += 5 # a = a + 5
+print(a)
+
+a -= 5 # a = a - 5
+print(a)
+
+a *= 5 # a = a * 5
+print(a)
+
+a /= 5 # a = a / 5
+print(a)
+
+a %= 5 # a = a % 5
+print(a)
+
+a **= 5 # a = a ** 5
+print(a)
+
+a //= 5 # a = a // 5
+print(a)
