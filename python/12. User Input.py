a0eab25e0e8becf9238cbfbbe944e463f0a53a16
Pranay <praveenpranay07@gmail.com>
2025-12-30 12:00:55 +0530

    Learnt about how to take user input in python ,using input function.

---
+++ python/12. User Input.py
@@ -0,0 +1,8 @@
+username = input("Enter your username: ")
+print("Welcome", username)
+
+#Sum of two numbers by taking User Input:
+num1 = int(input("Enter first number: ")) # Type casting
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
