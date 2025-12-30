2f2ea9220ce1b085638c78abeed6aa32d25961b1
Pranay <praveenpranay07@gmail.com>
2025-12-30 10:49:25 +0530

    Learnt about Keywords and Comments

---
+++ python/4. Keywords and Comments.py
@@ -0,0 +1,15 @@
+#Learning about Keywords and Comments
+
+False = 0 # False is a keyword,cannot be assigned as a variable
+True = 1 # True is a keyword,cannot be assigned as a variable
+
+print(type(False))
+print(type(True))
+
+'''
+
+This ia a multi
+line
+comment
+
+'''
