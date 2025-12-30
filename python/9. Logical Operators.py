20641ee5a64e63e4b618d0f6b7965531747ad4c6
Pranay <praveenpranay07@gmail.com>
2025-12-30 11:30:37 +0530

    Learnt about the different Logical Operators

---
+++ python/9. Logical Operators.py
@@ -0,0 +1,8 @@
+
+print((10 > 5) and (10 < 20)) # True
+print((3 > 5) and (10 < 20)) # False
+
+print((10 > 5) or (10 < 20)) # True
+print((3 > 5) or (10 < 20)) # True
+
+print(not (10 > 5)) # False
