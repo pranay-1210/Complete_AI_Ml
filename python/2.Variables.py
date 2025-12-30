ca9c6036d6620296308247ebed0f33e6cb19a92f
Pranay <praveenpranay07@gmail.com>
2025-12-30 10:28:51 +0530

    Learnt about Variables

---
+++ python/2.Variables.py
@@ -0,0 +1,16 @@
+name = "pranay"
+age = 23
+PI = 3.14
+
+print(name)
+print(age)
+print(PI)
+
+_name = "john"
+_age = 25
+
+print(_name)
+print(_age)
+
+# 2age = "Doe"
+# @name = "Jane"   Invalid variable names
