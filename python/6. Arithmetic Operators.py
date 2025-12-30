a5a5c27d35b1858b7f34b34c9e4a77112cfa61c0
Pranay <praveenpranay07@gmail.com>
2025-12-30 11:06:18 +0530

    Learnt about the different arithmetic operators.

---
+++ python/6. Arithmetic Operators.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)  # modulo division,returns the remainder
+print(a // b) # floor division
+print(a ** b) # exponent
