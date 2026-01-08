de09b117912bde0fe86b83f12b3c99ccdc9fce39
Pranay <praveenpranay07@gmail.com>
2026-01-08 16:38:58 +0530

    Learnt about the two basic methods of tuple i.e, index and count.

---
+++ python/27. Tuple Methods.py
@@ -0,0 +1,9 @@
+# Index method :- Returns the index of the first occurrence of the specified element
+
+my_tuple = (1, 2, 3, 4, 3, 5)
+print(my_tuple.index(3)) # 2
+
+# Count method :- Returns the number of times the specified element appears in the tuple
+
+my_tuple = (1, 2, 3, 4, 2, 5)
+print(my_tuple.count(3)) # 1
