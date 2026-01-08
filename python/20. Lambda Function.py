58362bfc527da28c862bb07d2c16c68a6e8c92c9
Pranay <praveenpranay07@gmail.com>
2026-01-08 12:54:52 +0530

    Learnt about Lamba Functions of Python.

---
+++ python/20. Lambda Function.py
@@ -0,0 +1,19 @@
+# Lambda Functions are anonymous functions,that can take any number of arguments, but can only have one expression.
+
+# Syntax:- lambda arguments: expression
+
+add = lambda x, y: x + y
+
+print(add(10, 20))
+
+# Example:-
+
+multiply = lambda x, y: x * y
+
+print(multiply(10, 20))
+
+# Example:-
+
+square = lambda x: x * x
+
+print(square(5))
